0240d228df3365bcd25a8db433aeb101d4453e69
Airik Farley <eric@ericfarley.net>
2023-05-16 09:23:32 -0700

    Material works everywhere except storybook

--- projects/header/src/lib/header.module.ts
+++ projects/header/src/lib/header.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { CommonModule } from '@angular/common';
-
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,6 +9,7 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
   ],
   exports: [
     HeaderComponent
