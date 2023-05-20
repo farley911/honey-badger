06f3f71c06167a7c80e3556b5fc67aad36351d8a
Airik Farley <eric@ericfarley.net>
2023-05-19 18:31:20 -0700

    Adds logo project to library

---
+++ projects/logo/src/lib/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-logo',
+  template: `
+    <p>
+      logo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LogoComponent {
+
+}
---
+++ projects/logo/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of logo
+ */
+
+export * from './lib/logo.service';
+export * from './lib/logo.component';
+export * from './lib/logo.module';
